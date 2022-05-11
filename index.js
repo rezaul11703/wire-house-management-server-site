e737f4ec658cd459848775223d2b4926ca270110
rezaul11703 <rezaulkarim11703@gmail.com>
2022-05-11 23:53:47 +0600

    Invetory Sectio Updated

--- index.js
+++ index.js
@@ -25,7 +25,59 @@ app.listen(port, ()=>{
   console.log('The Server is running on port no.', port)
 })
 
-}
+      const itemsInfo= client.db("ManageItems").collection('ALL Inventories')
+      const homeProductInfo= client.db("ManageItems").collection('dashboardProduct')
+        app.post('/addedItems',async(req,res)=>{
+          const newItem= req.body
+          const result= await itemsInfo.insertOne(newItem)
+          console.log(result)
+          res.send(result)
+        })
+        app.post('/displayProduct', async(req,res)=>{
+          const itemNumber= req.body
+          const result=await homeProductInfo.insertOne(itemNumber)
+          res.send(result)
+        })
+        app.get('/displayProduct',async(req,res)=>{
+          const query={}
+          const cursor=homeProductInfo.find(query)
+          const users= await cursor.limit(6).toArray()
+          res.send(users)
+        })
+        ///For Getting New Items
+        app.get('/addedItems',async(req,res)=>{
+          const query={}
+          const cursor=itemsInfo.find(query)
+          const users= await cursor.toArray()
+          res.send(users)
+        })
+        
+        app.get('/inventory/:id',async(req,res)=>{
+          const id=req.params.id
+          const query={_id:ObjectId(id)}
+          const result= await homeProductInfo.findOne(query)
+          res.send(result)
+    })
+    app.put('/inventory/:id', async(req,res)=>{
+      const id=req.params.id
+      const updatedUser=req.body
+      const  query={_id:ObjectId(id)}
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity:updatedUser.finalUser,
+        },
+      };
+      const result = await homeProductInfo.updateOne(query,updateDoc,options)
+      res.send(result)
+    })
+        app.delete('/inventory/:id',async(req,res)=>{
+          const id=req.params.id
+          const query={_id:ObjectId(id)}
+          const result= await itemsInfo.deleteOne(query)
+          res.send(result)
+    })
+  }
     finally{
 
     }
